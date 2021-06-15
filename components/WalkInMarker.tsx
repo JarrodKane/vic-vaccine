72192893e7da190c69dfed7bb9df76520cb7e461
JarrodKane <kajarrod@gmail.com>
2021-06-15 10:36:10 +1000

    Added in some interfaces for typescript and cleaned up the code

--- components/WalkInMarker.tsx
+++ components/WalkInMarker.tsx
@@ -4,60 +4,121 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 
-// enum waitPeriod {
-//   -1 = "a",
-//   beta = "b",
-// }
+// TODO: Need to work on the interface and types, remove any mention of any, and desctrcutre here
 
-// interface Clinic {
-//   _id: number;
-//   id: number;
-//   clinicId: string;
-//   shortName: string;
-//   location: string;
-//   address: string;
-//   suburb: string;
-//   addressFull: string;
-//   directions: string;
-//   lat: number;
-//   lng: number;
-//   lastUpdated: string;
-//   clinicStatus: string;
-//   hours: string;
-//   waitPeriod: string;
-//   waitPeriodDisp: string;
-//   shortNameClean: string;
-// }
+export interface ID {
+  $t: string;
+}
+
+export interface Category {
+  scheme: string;
+  term: string;
+}
+export interface Title {
+  type: TitleType;
+  $t: string;
+}
+
+export enum TitleType {
+  Text = "text",
+}
+
+export interface Link {
+  rel: string;
+  type: LinkType;
+  href: string;
+}
+export enum LinkType {
+  ApplicationAtomXML = "application/atom+xml",
+}
+
+export interface Entry {
+  // id: ID;
+  [key: string]: ID;
+  // category: Category[];
+  // link: Link[];
+  updated: ID;
+  title: Title;
+  content: Title;
+  gsx$id: ID;
+  gsx$clinicid: ID;
+  gsx$shortname: ID;
+  gsx$location: ID;
+  gsx$address: ID;
+  gsx$suburb: ID;
+  gsx$addressfull: ID;
+  gsx$directions: ID;
+  gsx$lat: ID;
+  gsx$lng: ID;
+  gsx$lastupdated: ID;
+  gsx$clinicstatus: ID;
+  gsx$hours: ID;
+  gsx$waitperiod: ID;
+  gsx$waitperioddisp: ID;
+  gsx$shortnameclean: ID;
+}
 
 interface Props {
-  clinic: any;
+  clinic: Entry;
+}
 
+interface Clinic {
+  id: string;
+  clinicid: string;
+  shortname: string;
+  location: string;
+  address: string;
+  suburb: string;
+  addressfull: string;
+  directions: string;
+  // lat: number;
+  // lng: number;
+  lastupdated: string;
+  clinicstatus: string;
+  hours: string;
+  waitperiod: string;
+  waitperioddisp: string;
+  shortnameclean: string;
+  [key: string]: string;
 }
 
 const WalkInMarker = (props: Props) => {
-  const { clinic } = props.clinic;
+  const gsx: any = {
+    // id: "",
+    // clinicid: "",
+    // shortname: "",
+    // location: "",
+    // address: "",
+    // suburb: "",
+    // addressfull: "",
+    // directions: "",
+    // lat: 0,
+    // lng: 0,
+    // lastupdated: "",
+    // clinicstatus: "",
+    // hours: "",
+    // waitperiod: "",
+    // waitperioddisp: "",
+    // shortnameclean: "",
+  };
 
-  const gsx = {};
-  
   // Destructuring the ugly clinic info so it's more easily used, first skipping all the default information and only adding the clinic specific info
+
   for (const key in props.clinic) {
-    // console.log(props.clinic.[key].$t);
     if (key.substring(0, 4) === "gsx$") {
-      const newKey = key.slice(4);
-      gsx.[newKey] = props.clinic[key].$t
+      let newKey = key.slice(4);
+      gsx[newKey] = props.clinic[key].$t;
     }
   }
 
-  console.log(gsx)
-
   return (
     <Marker position={[gsx.lat, gsx.lng]}>
-      
-      <Popup >
-      <div className={`whitespace-pre `}>{`${gsx.shortname}
+      <Popup>
+        <div className={`whitespace-pre `}>
+          {`${gsx.shortname}
 ${gsx.clinicstatus}
 ${gsx.addressfull}`}
-      </div>
+        </div>
       </Popup>
     </Marker>
   );
